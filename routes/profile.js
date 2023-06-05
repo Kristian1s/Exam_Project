bf05bfc4ea600d997d0071227459be42de39d934
Kristian <kristiansatree@gmail.com>
2023-06-06 00:34:02 +0200

    minor profile changes

--- routes/profile.js
+++ routes/profile.js
@@ -15,24 +15,31 @@ var RatingService = require("../services/ratingService");
 var ratingService = new RatingService(db);
 const { requiresAuth } = require('express-openid-connect');
 const UserService = require('../services/userService');
+const { render } = require('ejs');
 const userService = new UserService(db);
-/* GET home page. */
-router.get('/', requiresAuth(), async function(req, res, next) {
-  const movies = await movieService.getAll();
-  res.render('profile', { title: 'MovieVault', Movies: movies, isAuthenticated: req.oidc.isAuthenticated()});
-}
-);
 
-router.get('/:username', requiresAuth(), async function(req, res, next) {
+
+ router.get('/', requiresAuth(), async function(req, res, next) {
   const movies = await movieService.getAll();
-  const username = req.params.username;
-  let findUser = await userService.findOne(username);
-  if(findUser){
-    res.render('profile', {user: findUser, isAuthenticated: req.oidc.isAuthenticated(), Movies: movies })
-  }
+  /*  const user = await userService.find()  */
   res.render('profile', { title: 'MovieVault', Movies: movies, isAuthenticated: req.oidc.isAuthenticated()});
 }
-);
+); 
+
+ router.get('/:username', requiresAuth(), async function(req, res, next) {
+  const movies = await movieService.getAll();
+  const user = req.params;
+  if(!user){
+    res.redirect("/login")
+  }
+  const username = req.params.username;
+  let findUser = await userService.find(username);
+  if(findUser){
+    res.render('profile', {UserFromDb: findUser, isAuthenticated: req.oidc.isAuthenticated(), Movies: movies })
+  }
+  res.render('profile', {UserFromDb: "noUser",  title: 'MovieVault', Movies: movies, isAuthenticated: req.oidc.isAuthenticated()});
+}
+); 
 
 router.post('/', requiresAuth(), async function(req, res, next) {
   const { nickname, age, bio, avatar } = req.body;
